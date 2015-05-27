97f42f5952424a049a98a741558cca5290fa4ce5
Christian Ertler <christian@ertler.org>
2015-05-27 10:37:19 +0200

    add boost

--- code/source/main.cpp
+++ code/source/main.cpp
@@ -4,27 +4,32 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#include <boost/random.hpp>
+#include <boost/random/random_device.hpp>
+
+#include <boost/filesystem.hpp>
+
 int main(int argc, char** argv)
 {
-#ifdef _WIN32
-  const std::string imagePath = "..\\..\\data\\MIA_KU_2015_DataSet\\train-volume0001.tif";
-#else
-  const std::string imagePath = "../../MIA_KU_2015_DataSet/train-volume0001.tif";
-#endif
- 
-  cv::Mat image;
-  image = cv::imread(imagePath.c_str(), CV_LOAD_IMAGE_COLOR);   // Read the file
+    boost::filesystem::path imagePath("../../data/MIA_KU_2015_DataSet/train-volume0001.tif");
 
-  if(!image.data ) // Check for invalid input
-  {
-    std::cout <<  "Could not open or find the image " << imagePath.c_str() << std::endl ;
-    return EXIT_FAILURE;
-  }
+    boost::random::random_device rng;
+    boost::random::uniform_int_distribution<> uniform_dist(1, 6);
+    std::cout << "Random number: " << uniform_dist(rng) << std::endl;
 
-  cv::namedWindow( "Display window", CV_WINDOW_AUTOSIZE );// Create a window for display.
-  cv::imshow( "Display window", image );                  // Show our image inside it.
+    cv::Mat image;
+    image = cv::imread(imagePath.c_str(), CV_LOAD_IMAGE_COLOR);   // Read the file
 
-  cv::waitKey(0);                                         // Wait for a keystroke in the window
-  
-  return EXIT_SUCCESS;
+    if (!image.data) // Check for invalid input
+    {
+        std::cout << "Could not open or find the image " << imagePath.c_str() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    cv::namedWindow("Display window", CV_WINDOW_AUTOSIZE);// Create a window for display.
+    cv::imshow("Display window", image);                  // Show our image inside it.
+
+    cv::waitKey(0);                                         // Wait for a keystroke in the window
+
+    return EXIT_SUCCESS;
 }
