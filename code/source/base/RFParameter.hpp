91301c493d3c56d3b4a405c4655b54781815a6d2
Narcanti <bernhard.rapp@student.tugraz.at>
2015-06-02 15:20:54 +0200

    add: getter, setter, constructor

--- code/source/base/RFParameter.hpp
+++ code/source/base/RFParameter.hpp
@@ -10,14 +10,44 @@
 /// class RFParameter - 
 template <typename LABEL_TYPE, typename DATA_TYPE>
 class RFParameter {
-  // Associations
-  RTParameter* m_treeParams;
   // Attributes
 public:
-  bool m_bagging;
+	RFParameter()
+	{
+		m_bagging = true;
+	}
+
+	RFParameter(bool bagging, RTParameter treeParams, std::function < double(std::vector < Histrogram<LABEL_TYPE, DATA_TYPE)> ensemble_fct)
+	{
+		m_bagging = bagging;
+		m_treeParams = treeParams;
+		m_ensemble_fct = ensemble_fct;
+	}
+
+	RTParameter getTreeParams()
+	{
+		return m_treeParams;
+	}
+
+	void setTreeParams(RTParameter& treeParams)
+	{
+		m_treeParams = treeParams;
+	}
+
+	bool getBagging()
+	{
+		return m_bagging;
+	}
+
+	void setBagging(bool bagging)
+	{
+		m_bagging = bagging;
+	}
+
 private:
   RTParameter m_treeParams;
-  std::function<double(std::vector<Histogram<LABEL_TYPE, DATA_TYPE)> m_ensemble_fct;
+  std::function<double(std::vector<Histrogram<LABEL_TYPE, DATA_TYPE)> m_ensemble_fct;
+  bool m_bagging;
 };
 
 #endif
--- code/source/base/RTParameter.hpp
+++ code/source/base/RTParameter.hpp
@@ -7,6 +7,33 @@ class RTParameter {
 private:
   size_t m_maxDepth;
   size_t m_minSamples;
+
+public:
+	RTParameter(size_t maxDepth, size_t minSamples)
+	{
+		m_maxDepth = maxDepth;
+		m_minSamples = minSamples;
+	}
+
+	size_t getMaxDepth()
+	{
+		return m_maxDepth;
+	}
+
+	void setMaxDepth(size_t maxDepth)
+	{
+		m_maxDepth = maxDepth;
+	}
+
+	size_t getMinSamples()
+	{
+		return m_minSamples;
+	}
+
+	void setMinSamples(size_t minSamples)
+	{
+		m_minSamples = minSamples;
+	}
 };
 
 #endif
