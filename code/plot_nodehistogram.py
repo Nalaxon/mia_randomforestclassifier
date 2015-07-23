fed8463fac64fafbc82e1384ac4f9a5f8ddaacde
Christian Ertler <christian@ertler.org>
2015-07-23 19:50:10 +0200

    add python script to plot the node histogram

---
+++ code/plot_nodehistogram.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python2
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pylab
+
+dot_file = "bin/trees.txt"
+
+
+if __name__ == "__main__":
+	histogram = {
+		'CenterPixelNode': 0,
+		'GradientNode': 0,
+		'TwoPixelNode': 0,
+		'HaarWaveletNode': 0,
+		'SURFFilterNode': 0,
+		'TwoRegionsNode': 0,
+		'SumNode': 0,
+		'TwoPixelGradientNode': 0
+	}
+
+	with open(dot_file) as f:
+		file_data = "".join([line for line in f])
+		count_total = 0
+		for key in histogram:
+			count = file_data.count(key)
+			count_total += count
+			histogram[key] = count
+
+		fig, ax = plt.subplots()
+		plt.subplots_adjust(left=0.25, right=0.88)
+		pos = np.arange(len(histogram)) + 0.5
+		names = []
+		counts = []
+		for name, count in histogram.iteritems():
+			names.append(name)
+			counts.append(count)
+		counts = np.array(counts, dtype=np.float) / count_total
+		ax.barh(pos, counts, align='center', color='m')
+		pylab.yticks(pos, names)
+		plt.title('Distribution of chosen weak learners')
+		plt.xlabel('%')
+		plt.draw()
+		plt.show()
