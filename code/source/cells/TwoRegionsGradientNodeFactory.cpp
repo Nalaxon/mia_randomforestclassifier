70435cc71c1d3305eb47810b652943533f73ceb5
Narcanti <bernhard.rapp@student.tugraz.at>
2015-10-05 11:54:55 +0200

    make sure not violate bounce conditions

--- code/source/cells/TwoRegionsGradientNodeFactory.cpp
+++ code/source/cells/TwoRegionsGradientNodeFactory.cpp
@@ -2,28 +2,18 @@
 #include "TwoRegionsGradientNode.hpp"
 #include "NodeFactory.hpp"
 #include "memory.hpp"
-
+#include "tools\ImageTools.hpp"
 TwoRegionsGradientNodeFactory::NodePtr TwoRegionsGradientNodeFactory::createRandomNode(std::ostream* log_stream)
 {
-    cv::Rect region1(m_col_dist(m_rng), m_row_dist(m_rng), m_row_dist(m_rng), m_col_dist(m_rng));
-    cv::Rect region2(m_col_dist(m_rng), m_row_dist(m_rng), m_row_dist(m_rng), m_col_dist(m_rng));
-    adjust_region(region1);
-    adjust_region(region2);
+	cv::Rect region1(m_col_start_dist(m_rng), m_row_start_dist(m_rng),
+		m_row_width_dist(m_rng), m_col_height_dist(m_rng));
+	cv::Rect region2(m_col_start_dist(m_rng), m_row_start_dist(m_rng),
+		m_row_width_dist(m_rng), m_col_height_dist(m_rng));
+	ImageTools::adjust_region(region1, m_params.patch_height, m_params.patch_width);
+	ImageTools::adjust_region(region2, m_params.patch_height, m_params.patch_width);
     float threshold = m_threshold_dist(m_rng);
 	return std::make_unique<TwoRegionsGradientNode>(region1, region2, threshold, log_stream);
 }
 
-void TwoRegionsGradientNodeFactory::adjust_region(cv::Rect& region)
-{
-    if ((region.x + region.width) >= m_params.patch_width)
-    {
-        region.width = m_params.patch_width - region.x - 1;
-    }
-    
-    if ((region.y + region.height) >= m_params.patch_height)
-    {
-        region.height = m_params.patch_height - region.y - 1;
-    }
-}
 
 
--- code/source/cells/TwoRegionsGradientNodeFactory.hpp
+++ code/source/cells/TwoRegionsGradientNodeFactory.hpp
@@ -12,7 +12,11 @@ class TwoRegionsGradientNodeFactory : public NodeFactory<CellLabel, cv::Mat>
 {
 public:
 	TwoRegionsGradientNodeFactory(const PatchParameter& params)
-  : m_params(params), m_row_dist(0, params.patch_width-1), m_col_dist(0, params.patch_height-1), m_threshold_dist(0., 1.)
+		: m_params(params), m_row_start_dist(0, params.patch_width - 2),
+		m_col_start_dist(0, params.patch_height - 2),
+		m_row_width_dist(1, params.patch_width - 1),
+		m_col_height_dist(1, params.patch_height - 1),
+		m_threshold_dist(0., 1.)
   {
   }
   
@@ -25,10 +29,10 @@ private:
   
   PatchParameter m_params;
   
-  boost::random::uniform_int_distribution<> m_row_dist, m_col_dist;
+  boost::random::uniform_int_distribution<> m_row_start_dist, m_col_start_dist,
+	  m_row_width_dist, m_col_height_dist;
+
   boost::random::uniform_real_distribution<> m_threshold_dist;
-  
-  void adjust_region(cv::Rect& region);
 } ;
 
 #endif	/* TWOREGIONSGRADIENTNODEFACTORY_HPP */
