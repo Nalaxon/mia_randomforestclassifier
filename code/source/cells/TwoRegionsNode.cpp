73dc73a74ab36dcb70190b0769c9a933382ecca7
Narcanti <bernhard.rapp@student.tugraz.at>
2015-10-05 11:57:31 +0200

    extent logging information

--- code/source/cells/TwoRegionsNode.cpp
+++ code/source/cells/TwoRegionsNode.cpp
@@ -7,7 +7,8 @@ TwoRegionsNode::Direction TwoRegionsNode::split(const cv::Mat& mat) const
     float mean2 = ImageTools::mean_from_integral<float, 4, 2>(mat, m_region2);
     
 	if (m_log_stream != nullptr)
-		*m_log_stream << "TwoRegionsNode;" << (mean1 - mean2) << ";" << std::endl;
+		*m_log_stream << "TwoRegionsNode;" << (mean1 - mean2) << ";" << mean1 << ";" << mean2 << ";"
+		<< m_region1.x << ";" << m_region1.y << ";" << m_region1.width << ";" << m_region1.height << std::endl;
 	
 	if ((mean1 - mean2) < m_threshold)
     {
