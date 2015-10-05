67c7fdd415d7fa9edd1ebf69c18ed990c966f7ec
Narcanti <bernhard.rapp@student.tugraz.at>
2015-10-05 11:51:34 +0200

    force to abs value

--- code/source/cells/TwoRegionsGradientNode.cpp
+++ code/source/cells/TwoRegionsGradientNode.cpp
@@ -9,7 +9,7 @@ TwoRegionsGradientNode::Direction TwoRegionsGradientNode::split(const cv::Mat& m
 	cv::split(mat(m_region2), mat2);
 	float mean1 = ImageTools::mean_from_integral<float, 4, 2>(mat, m_region1);
 	float mean2 = ImageTools::mean_from_integral<float, 4, 2>(mat, m_region2);
-	double sub = mean1 - mean2;
+	double sub = abs(mean1 - mean2);
 	
 	if (m_log_stream != nullptr)
 		*m_log_stream << "TwoRegionsGradientNode;" << sub << std::endl;
